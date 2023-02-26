c971da5e97625cd818bfdc2a1a33bbe82fd18ed3
k905606 <k906506@naver.com>
2023-02-26 20:53:55 +0900

    [Update] 프로젝트 초기 세팅

---
+++ PreOnBoarding-iOS-Challenge-March/PreOnBoarding-iOS-Challenge-March/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreOnBoarding-iOS-Challenge-March
+//
+//  Created by 황교범 on 2023/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
